b62ffc74fadf01251dbd6bd75f4bb833eb43fc62
Gulsemin113 <77548396+Gulsemin113@users.noreply.github.com>
2021-10-11 22:18:45 -0500

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -18,4 +18,23 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!")
+
+
+const whichSchool  = function (age) {
+  // For ages 1 to 12 returns Elementary School
+  if(age >= 13 && age < 19){
+    console.log("Secondary School");
+    //For ages 13 to 18 returns Secondary School
+  }else if(age >= 19 && age <= 100){
+    console.log(" LightHouse Labs");
+    //For ages 19 to let's say 100 returns Lighthouse Labs
+  }else if (age >= 1 && age < 13){
+    console.log("Elementary School");
+  }
+}
+ 
+console.log("I am 19. Which school should I go to?");
+console.log(whichSchool(19));
+
+
+
