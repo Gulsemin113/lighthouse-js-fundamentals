2f1a951523648135120dc6de614df5f53f794fdd
Gulsemin113 <77548396+Gulsemin113@users.noreply.github.com>
2021-10-14 16:30:36 -0400

    Create array.js

---
+++ array.js
@@ -0,0 +1,14 @@
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+
+console.log("My favourite " + director + " film is " + films[1]);
+
+console.log("films at index 0: " + films[0]);
+console.log("films at index 1: " + films[1]);
+console.log("films at index 2: " + films[2]);
+console.log("films at index 3: " + films[3]);
+
+films[2] = "Jurassic Park";
+films;
+films.push("A.I.");
+films;
