78aa9bb7c79b4b31a268de0dc6224741cf3133fe
Gulsemin113 <77548396+Gulsemin113@users.noreply.github.com>
2021-10-14 18:15:04 -0400

    Update lastIndexOf.js

--- lastIndexOf.js
+++ lastIndexOf.js
@@ -2,7 +2,7 @@ const lastIndexOf = (array,value) => {
 
   for(let i = array.length ; i > 0 ; i--) {
 
-    if(array[i-1] === value) {
+    if(array [i - 1] === value) {
       return i - 1;
     }
   }
